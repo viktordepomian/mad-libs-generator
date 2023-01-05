02b39bbd81425742383d173b4db8c26dd5945b28
viktordepomian <viktor.depomian@gmail.com>
2023-01-05 23:21:46 +0100

    Added all new files

--- mad-libs.py
+++ mad-libs.py
@@ -1,6 +1,12 @@
-name = "Viktor"
+name = input("Please input a name: ")
+food = input("Please input a type of food: ")
+adjective = input("Please input a adjective: ")
+noun = input("Please input a noun: ")
+verb2 = input("Please input a verb: ")
+verb3 = input("Please input a verb: ")
+verb1 = input("Please input a verb: ")
 
-story = f"It was {food} day at school, and {name} was super ___(ADJECTIVE)___ for lunch. But when she went outside to eat, a ___(NOUN)___ stole her $food! $name chased the ___(NOUN)___ all over school. She ___(VERB1)___, ___(VERB2)___, and ___(VERB3)___ through the playground. Then she tripped on her ___(NOUN)___ and the ___(NOUN)___ escaped! Luckily, $name’s friends were willing to share their $food with her."
+story = f"It was {food} day at school, and {name} was super {adjective} for lunch. But when she went outside to eat, an {noun} guy stole her {food}! {name} chased the {noun} all over school. She {verb1}, {verb2}, and {verb3} through the playground. Then she tripped on her {noun} and the {noun} escaped! Luckily, {name}’s friends were willing to share their {food} with her."
 
 
 print(story)
