36a34f951703816efcb475bfbd54bde74dec13b1
viktordepomian <viktor.depomian@gmail.com>
2023-01-05 11:37:55 +0100

    Updated files

---
+++ mad-libs.py
@@ -0,0 +1,6 @@
+name = "Viktor"
+
+story = f"It was {food} day at school, and {name} was super ___(ADJECTIVE)___ for lunch. But when she went outside to eat, a ___(NOUN)___ stole her $food! $name chased the ___(NOUN)___ all over school. She ___(VERB1)___, ___(VERB2)___, and ___(VERB3)___ through the playground. Then she tripped on her ___(NOUN)___ and the ___(NOUN)___ escaped! Luckily, $name’s friends were willing to share their $food with her."
+
+
+print(story)
